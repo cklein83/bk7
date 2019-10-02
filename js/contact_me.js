14b77820015706d7e57714c7da875ce3efed583e
Christopher Klein <christopher.klein@bechtle.com>
2019-10-02 09:10:20 +0200

    form

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,7 +17,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                //url: "././mail/contact_me.php",
+                url: "/",
                 type: "POST",
                 data: {
                     name: name,
@@ -31,7 +32,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Die Nachricht wurde gesendet. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -43,7 +44,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Entschuldigen Sie, es ist ein Fehler aufgetreten. Versuchen Sie es später noch einmal.");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
